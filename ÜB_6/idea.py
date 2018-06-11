9726bc892302ac3d6555a4211dde7e06b2e3639c
Martin Michael Zier <martin.michael.zier@artcom.de>
2018-06-11 21:20:04 +0200

    This is someone else commiting in your repository

---
+++ ÜB_6/idea.py
@@ -0,0 +1,20 @@
+# Head start.
+
+def count_or_add_trigrams(trigram, trigrams_so_far):
+    '''
+    Takes a trigram, and a list of previously seen trigrams
+    and yields the same list with all discovered and counted
+    trigrams.
+    Adds given trigram if not found,
+    increments the trigram counter if found.
+    '''
+    found = False
+    for entry in trigrams_so_far:
+        test_trigram = entry[0]
+        if test_trigram == trigram:
+            entry[1] += 1
+            found = True
+            break
+    if not found:
+        trigrams_so_far.append([trigram, 1])
+    return trigrams_so_far
