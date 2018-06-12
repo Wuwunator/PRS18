f9afc68f0e862d8146103ceb91c78b62016f199d
Martin Michael Zier <martin.michael.zier@artcom.de>
2018-06-12 07:33:06 +0200

    Adding example case, just to be sure.

--- ÜB_6/idea.py
+++ ÜB_6/idea.py
@@ -1,6 +1,10 @@
 # Head start.
 # ask me for this solution: 6cb9ce6024b5fd41aebb86ccd40d8080
 
+# this line is not needed, just for better output:
+from pprint import pprint
+# just remove the top line
+
 def count_or_add_trigrams(trigram, trigrams_so_far):
     '''
     Takes a trigram, and a list of previously seen trigrams
@@ -19,3 +23,25 @@ def count_or_add_trigrams(trigram, trigrams_so_far):
     if not found:
         trigrams_so_far.append([trigram, 1])
     return trigrams_so_far
+
+
+test_trigrams = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['b', 'd', 'e'],
+    ['d', 'e', 'f'],
+    ['a', 'a', 'a'],
+    ['d', 'e', 'f']
+]
+
+trigram_count = []
+for trigram in test_trigrams:
+    print('I have been given this trigram:', end=' ')
+    pprint(trigram)
+    count_or_add_trigrams(trigram, trigram_count)
+    print('After finishing this operation, my data looks like:')
+    pprint(trigram_count)
+    print('-------------------------------------------------------------')
+
+print('After doing all test trigrams, this is what I have:')
+pprint(trigram_count)
