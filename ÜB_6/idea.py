5ac2ca625edf3247923f7b2aed4634e1b4b007cf
Martin Michael Zier <martin.michael.zier@artcom.de>
2018-06-12 11:16:35 +0200

    break allows for else loops

--- ÜB_6/idea.py
+++ ÜB_6/idea.py
@@ -13,14 +13,12 @@ def count_or_add_trigrams(trigram, trigrams_so_far):
     Adds given trigram if not found,
     increments the trigram counter if found.
     '''
-    found = False
     for entry in trigrams_so_far:
         test_trigram = entry[0]
         if test_trigram == trigram:
             entry[1] += 1
-            found = True
             break
-    if not found:
+    else:
         trigrams_so_far.append([trigram, 1])
     return trigrams_so_far
 
